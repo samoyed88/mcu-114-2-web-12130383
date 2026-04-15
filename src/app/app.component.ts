0ec1ce7315d972c8777f6824dcda5076a3c74dce
samoyeddmsc <ice08081001@gmail.com>
2026-04-15 18:19:32 +0800

    feat(router): 切換產品清單與登入頁面實作

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { HeaderComponent } from './header/header.component';
 import { Component } from '@angular/core';
-import { ProductCardListComponent } from './product-card-list/product-card-list.component';
-import { Product } from './model/product';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
