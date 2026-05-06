ba18d0b1061b15b39a440adc84c18802471f0936
samoyeddmsc <ice08081001@gmail.com>
2026-05-06 17:52:13 +0800

    feat(resolver): 建立產品資料預載資料

--- src/app/product-detail-page/product-detail-page.component.ts
+++ src/app/product-detail-page/product-detail-page.component.ts
@@ -11,17 +11,11 @@ import { ProductService } from '../services/product.service';
   styleUrl: './product-detail-page.component.scss',
 })
 export class ProductDetailPageComponent {
-  id = input.required<number, string | number>({ transform: numberAttribute });
-
-  protected readonly product = signal<Product>(new Product());
+  readonly product = input.required<Product>();
 
   private readonly router = inject(Router);
 
   private readonly productService = inject(ProductService);
-
-  ngOnInit(): void {
-    this.productService.getById(this.id()).subscribe((product) => this.product.set(product));
-  }
 
   onEdit(): void {
     this.router.navigate(['product', 'form', this.product().id]);
---
+++ src/app/resolver/product.resolver.ts
@@ -0,0 +1,11 @@
+import { inject } from '@angular/core';
+import { ResolveFn } from '@angular/router';
+
+import { ProductService } from '../services/product.service';
+import { Product } from '../model/product';
+
+export const productResolver: ResolveFn<Product> = (route) => {
+  const productService = inject(ProductService);
+  const id = route.paramMap.get('id')!;
+  return productService.getById(+id);
+};
