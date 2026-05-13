e6747250e3cc26c8920be47d2923bccf62d3aad6
samoyeddmsc <ice08081001@gmail.com>
2026-05-13 18:20:01 +0800

    feat(product): 實作從後端服務取得所有產品資料

---
+++ src/app/services/product-remote.service.ts
@@ -0,0 +1,18 @@
+import { inject, Injectable } from '@angular/core';
+import { ProductService } from './product.service';
+import { Product } from '../model/product';
+import { map, Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProductRemoteService extends ProductService {
+  private readonly url = 'http://localhost:3000/products';
+
+  private readonly httpClient = inject(HttpClient);
+
+  override getList(name: string | undefined, index: number, size: number): Observable<{ data: Product[]; count: number }> {
+    return this.httpClient.get<Product[]>(this.url).pipe(map((data) => ({ data, count: data.length })));
+  }
+}
