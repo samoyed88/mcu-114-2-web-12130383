93e8649963562c6839ac38710e8d24367b9f4806
samoyeddmsc <ice08081001@gmail.com>
2026-05-20 17:06:14 +0800

    feat(product-form): 產品表單新增作業實作

--- src/app/product-form-page/product-form-page.component.ts
+++ src/app/product-form-page/product-form-page.component.ts
@@ -1,7 +1,9 @@
-import { Component, input } from '@angular/core';
+import { Component, inject, input } from '@angular/core';
 import { Product } from '../model/product';
 import { FormArray, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { JsonPipe } from '@angular/common';
+import { Router } from '@angular/router';
+import { ProductService } from '../services/product.service';
 
 @Component({
   selector: 'app-product-form-page',
@@ -12,12 +14,17 @@ import { JsonPipe } from '@angular/common';
 export class ProductFormPageComponent {
   readonly product = input<Product>();
 
+  private readonly router = inject(Router);
+
+  private readonly productService = inject(ProductService);
+
   protected readonly form = new FormGroup({
     id: new FormControl<string | null>(null),
     name: new FormControl<string | null>(null, { validators: [Validators.required] }),
     authors: new FormArray<FormControl<string | null>>([]),
     company: new FormControl<string | null>(null, { validators: [Validators.required] }),
     price: new FormControl<number | null>(null, { validators: [Validators.required] }),
+    isShow: new FormControl<boolean>(true, { nonNullable: true }),
   });
 
   get name(): FormControl<string | null> {
@@ -36,8 +43,29 @@ export class ProductFormPageComponent {
     return this.form.get('price') as FormControl<string | null>;
   }
 
+  get isShow(): FormControl<boolean> {
+    return this.form.get('isShow') as FormControl<boolean>;
+  }
+
   protected onAddAuthor(): void {
     const formControl = new FormControl<string | null>(null, { validators: [Validators.required] });
     this.authors.push(formControl);
+  }
+
+  protected onSave(): void {
+    const formData = new Product({
+      name: this.name.value!,
+      authors: this.authors.value.map((author) => author!),
+      company: this.company.value!,
+      isShow: this.isShow.value,
+      photoUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
+      createDate: new Date(),
+      price: +(this.price.value || '0'),
+    });
+    this.productService.add(formData).subscribe(() => this.router.navigate(['products']));
+  }
+
+  protected onCancel(): void {
+    this.router.navigate(['products']);
   }
 }
